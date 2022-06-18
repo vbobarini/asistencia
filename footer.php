3e72cb5a95d07bc0449dfc91858c52e0cb6d19f6
CECILIA <vbobarini@gmail.com>
2022-06-18 01:28:20 -0300

    asistencia completo terminado con logo listo para reproducir y realizar cambios

---
+++ footer.php
@@ -0,0 +1,14 @@
+<?php
+?>
+</main>
+<footer class="px-2 py-2 fixed-bottom bg-dark">
+        <span class="text-muted">Attendance system written by
+            <a class="text-white" href="">Bienvenido a asistencia vcb </a>
+            &nbsp;|&nbsp;
+            <a target="_blank" class="text-white" href="">
+                View source
+            </a>
+        </span>
+</footer>
+</body>
+</html>
---
+++ nav.php
@@ -0,0 +1,27 @@
+<?php
+?>
+<nav class="navbar navbar-expand-md navbar-dark bg-success fixed-top">
+    <a class="navbar-brand" href="">
+        <img class="img-fluid" style="max-width: 200px" src="img\vcbytes.png " loading="lazy">
+    </a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" id="botonMenu" aria-label="Mostrar u ocultar menú">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="menu">
+        <ul class="navbar-nav mr-auto">
+            <li class="nav-item">
+                <a class="nav-link" href="employees.php">Employees <i class="fa fa-users"></i></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="attendance_register.php">Register attendance <i class="fa fa-check-double"></i></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="attendance_report.php">Attendance report <i class="fa fa-file-alt"></i></a>
+            </li>
+            <li class="nav-item active">
+                <a class="nav-link" href="">Support & help&nbsp;<i class="fa fa-hands-helping"></i></a>
+            </li>
+        </ul>
+    </div>
+</nav>
